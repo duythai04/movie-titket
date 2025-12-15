d429f0b63f01f5276a6aa6f43aca200d34bc32bf
duythai04 <thaiduyluong662@gmail.com>
2025-12-15 19:02:17 +0700

    update scss

--- frontend/src/components/Banner/Banner.jsx
+++ frontend/src/components/Banner/Banner.jsx
@@ -1,37 +1,40 @@
-import React from "react";
-import Slider from "react-slick";
-import "./Banner.scss";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import React from 'react';
+import Slider from 'react-slick';
+import './Banner.scss';
 
-function Banner() {
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const Banner = () => {
   const dataBanner = [
     {
-      img: "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2FBanner%2F0019497.png&w=2048&q=75",
+      img: 'https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2FBanner%2F0019497.png&w=2048&q=75',
     },
     {
-      img: "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2FBanner%2F0019485.jpg&w=2048&q=75",
+      img: 'https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2FBanner%2F0019485.jpg&w=2048&q=75',
     },
     {
-      img: "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2FBanner%2F0019423.jpg&w=2048&q=75",
+      img: 'https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2FBanner%2F0019423.jpg&w=2048&q=75',
     },
     {
-      img: "https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2FBanner%2F0019421.jpg&w=2048&q=75",
+      img: 'https://chieuphimquocgia.com.vn/_next/image?url=http%3A%2F%2Fapiv2.chieuphimquocgia.com.vn%2FContent%2FImages%2FBanner%2F0019421.jpg&w=2048&q=75',
     },
   ];
 
   const settings = {
     dots: true,
     infinite: true,
-    speed: 500,
+    speed: 600,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 3500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    arrows: true,
+    pauseOnHover: false,
   };
 
   return (
-    <div className="banner-container">
+    <section className="banner-container">
       <Slider {...settings}>
         {dataBanner.map((item, index) => (
           <div className="item-banner" key={index}>
@@ -39,8 +42,8 @@ function Banner() {
           </div>
         ))}
       </Slider>
-    </div>
+    </section>
   );
-}
+};
 
 export default Banner;
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TiTicket } from 'react-icons/ti';
 import { LuPopcorn } from 'react-icons/lu';
 import { IoSearchOutline } from 'react-icons/io5';
 import { FaRegUserCircle } from 'react-icons/fa';
 import { MdLanguage } from 'react-icons/md';
+import { LuMenu } from 'react-icons/lu';
 import { Link } from 'react-router-dom';
 import './Header.scss';
 
 function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
+
   return (
     <header className="header">
       <div className="header-top">
@@ -15,32 +18,49 @@ function Header() {
           <h1>Cinema</h1>
         </Link>
 
+        {/* Desktop */}
         <div className="header-actions">
           <Link to="/movies" className="btn btn-ticket">
             <TiTicket className="btn-icon" />
-            ĐẶT VÉ NGAY
+            ĐẶT VÉ
           </Link>
 
           <button className="btn btn-combo">
             <LuPopcorn className="btn-icon" />
-            ĐẶT BỎNG NƯỚC
+            BỎNG NƯỚC
           </button>
+
+          <div className="mobile-search">
+            <input type="text" placeholder="Tìm kiếm phim" />
+          </div>
         </div>
 
         <div className="header-icons">
-          <div className="icon-item">
-            <IoSearchOutline />
-          </div>
-
+          <IoSearchOutline className="icon-item" />
           <Link to="/register" className="icon-item">
             <FaRegUserCircle />
           </Link>
-
-          <div className="icon-item">
-            <MdLanguage />
-          </div>
+          <MdLanguage className="icon-item" />
         </div>
+
+        {/* Mobile menu button */}
+        <button className="menu-toggle" onClick={() => setOpenMenu(!openMenu)}>
+          <LuMenu />
+        </button>
       </div>
+
+      {/* Mobile menu */}
+      {openMenu && (
+        <div className="mobile-menu">
+          <Link to="/movies" onClick={() => setOpenMenu(false)}>
+            Phim
+          </Link>
+          <Link to="/register" onClick={() => setOpenMenu(false)}>
+            Tài khoản
+          </Link>
+          <button onClick={() => setOpenMenu(false)}>Bỏng nước</button>
+        </div>
+      )}
     </header>
   );
 }
