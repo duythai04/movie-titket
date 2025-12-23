47a7df490481cedacf80243f494ad0fd690c1f1f
duythai04 <thaiduyluong662@gmail.com>
2025-12-23 15:52:44 +0700

    fix api

--- backend/restfull-api/src/server.js
+++ backend/restfull-api/src/server.js
@@ -29,7 +29,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/showtimes', showtimeRoutes);
 app.use('/api/seats', seatRoutes);
 app.use('/api/combos', foodComboRoutes);
-app.use('/api/search', searchMovieRoutes);
+app.use('/api/api/search', searchMovieRoutes);
 
 const PORT = process.env.PORT;
 
